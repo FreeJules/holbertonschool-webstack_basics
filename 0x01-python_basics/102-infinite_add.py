98b617e583a90d767487e6c6372d68e8c7a7c4f5
Julija Lee <lee.julija@gmail.com>
2018-05-19 17:10:06 +0000

    adding infinite add

---
+++ 0x01-python_basics/102-infinite_add.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+from sys import argv
+
+
+def infinite_add():
+    s = 0
+    for i in range(1, len(argv)):
+        s += int(argv[i])
+    print(s)
+
+if __name__ == "__main__":
+    infinite_add()

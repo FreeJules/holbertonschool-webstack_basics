411d13c09296183213e750a54598ce9f80ce0424
Julija Lee <lee.julija@gmail.com>
2018-05-19 08:16:19 +0000

    moved for loop inside if

--- 0x01-python_basics/5-args.py
+++ 0x01-python_basics/5-args.py
@@ -7,8 +7,8 @@ if __name__ == "__main__":
     if total_args == 1:
         print('0 arguments.')
     elif total_args == 2:
-        print('1 argument:')
+        print('1 argument:\n1: {}'.format(argv[1]))
     else:
         print('{} arguments:'.format(total_args - 1))
-    for arg in range(1, total_args):
-        print ('{}: {}'.format(arg, argv[arg]))
+        for arg in range(1, total_args):
+            print ('{}: {}'.format(arg, argv[arg]))

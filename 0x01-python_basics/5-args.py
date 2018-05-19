5f939c316e76ffa831e770b9a55ff37cc462b982
Julija Lee <lee.julija@gmail.com>
2018-05-19 20:39:40 +0000

    removed hard coded arg number

--- 0x01-python_basics/5-args.py
+++ 0x01-python_basics/5-args.py
@@ -3,12 +3,12 @@ from sys import argv
 
 
 if __name__ == "__main__":
-    total_args = len(argv)
-    if total_args == 1:
-        print('0 arguments.')
-    elif total_args == 2:
-        print('1 argument:\n1: {}'.format(argv[1]))
+    total_args = len(argv) - 1
+    if total_args == 0:
+        print('{} arguments.'.format(total_args))
+    elif total_args == 1:
+        print('{} argument:\n1: {}'.format(total_args, argv[total_args]))
     else:
-        print('{} arguments:'.format(total_args - 1))
-        for arg in range(1, total_args):
+        print('{} arguments:'.format(total_args))
+        for arg in range(1, total_args + 1):
             print ('{}: {}'.format(arg, argv[arg]))

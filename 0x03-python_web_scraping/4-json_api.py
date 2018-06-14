78d555351dde8f3d3201c827f65eb3f33034b3ee
Julija Lee <lee.julija@gmail.com>
2018-06-14 20:26:01 +0000

    changed variable just to letter

--- 0x03-python_web_scraping/4-json_api.py
+++ 0x03-python_web_scraping/4-json_api.py
@@ -5,14 +5,15 @@ import sys
 if __name__ == "__main__":
     url = 'http://0.0.0.0:5000/search_user'
     if len(sys.argv) > 1:
-        q = {'letter': sys.argv[1]}
+        q = sys.argv[1]
     else:
         q = ""
     r = requests.post(url, data={'q': q})
-    j = r.json()
-    if not isinstance(j, dict):
+    try:
+        j = r.json()
+    except:
         print("Not a valid JSON")
-    elif not j:
+    if not j:
         print("No result")
     else:
         print("[{}] {}".format(j.get('id'), j.get('name')))

6906077f38eed34252c236b40bebed36ca71dbca
Julija Lee <lee.julija@gmail.com>
2018-06-27 11:52:09 +0000

    removed isNaN

--- 0x02-javascript_basics/11-writeme.js
+++ 0x02-javascript_basics/11-writeme.js
@@ -2,7 +2,7 @@
 let fs = require('fs');
 let fileName = process.argv[2];
 let content = process.argv[3];
-if (isNaN(content)) {
+if (!content) {
   content = '';
 }
 fs.writeFile(fileName, content, 'utf8', function (err) {

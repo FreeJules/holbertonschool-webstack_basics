570cc349d3f063de411a31a61496b5677a4c5cd2
Julija Lee <lee.julija@gmail.com>
2018-06-27 12:07:35 +0000

    checked length instead

--- 0x02-javascript_basics/11-writeme.js
+++ 0x02-javascript_basics/11-writeme.js
@@ -1,9 +1,11 @@
 #!/usr/bin/node
 let fs = require('fs');
 let fileName = process.argv[2];
-let content = process.argv[3];
-if (content === undefined) {
+let content;
+if (process.argv.length < 4) {
   content = '';
+} else {
+  content = process.argv[3];
 }
 fs.writeFile(fileName, content, 'utf8', function (err) {
   if (err) {

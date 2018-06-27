7aad221d334b0674ddc0c7b630d774d1e5f9e6cb
Julija Lee <lee.julija@gmail.com>
2018-06-27 05:11:05 +0000

    list index out of range for less than 10 commits

--- 0x03-python_web_scraping/7-github_commits.py
+++ 0x03-python_web_scraping/7-github_commits.py
@@ -8,6 +8,10 @@ if __name__ == '__main__':
     url = 'https://api.github.com/repos/' + owner + '/' + repo + '/commits'
     r = requests.get(url)
     j = r.json()
-    for i in range(10):
+    commits = 10
+    num_commits = len(j)
+    if num_commits < 10:
+        commits = num_commits
+    for i in range(commits):
         print('{}: {}'.format(j[i].get('sha'), j[i].get('commit')
                               .get('author').get('name')))
